4e11feeb61fc2d00affb6d852e9b4d4afaaaac58
Steve Coluccio <thecalooch@gmail.com>
2018-05-03 11:51:23 -0400

    huffman files

---
+++ hdec.cpp
@@ -0,0 +1,198 @@
+//STEPHEN COLUCCIO cs435 9136 mp
+//Option 1: Huffman coding, decode
+
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+class Node{
+public: 
+	int freq;
+	char byte_code;
+	Node * left_child;
+	Node * right_child;
+
+	Node(int f, char byte='\0', Node* left=NULL, Node* right=NULL): 
+		freq(f), byte_code(byte), left_child(left), right_child(right) {}
+	
+	~Node()
+	{
+		delete left_child;
+		delete right_child;
+	}
+};
+
+
+//Priorty Heap
+class PriorityHeap{
+public:
+	vector<Node*> heap;
+
+	PriorityHeap(){}
+	~PriorityHeap(){}
+
+	void insert(Node* element){
+		heap.push_back(element);
+		heapify_up(heap.size()-1);
+	}
+
+	Node* extract_min(){
+		Node* min = heap.front();
+		heap[0] = heap.at(heap.size()-1);
+		heap.pop_back();
+		heapify_down(0);
+		return min;
+	}
+
+	void heapify_up(int idx){
+		while(parent(idx) >= 0 && idx > 0 && heap[parent(idx)]->freq > heap[idx]->freq){
+			Node* temp = heap[parent(idx)];
+			heap[parent(idx)] = heap[idx];
+			heap[idx] = temp;
+			idx = parent(idx);
+		}
+	}
+
+	void heapify_down(int idx){
+		int child = left(idx);
+		if(child > 0 && right(idx) > 0 && heap[child]->freq > heap[right(idx)]->freq){
+			child = right(idx);
+		} 
+		if(child > 0){
+			Node* temp = heap[idx];
+			heap[idx] = heap[child];
+			heap[child] = temp;
+			heapify_down(child);
+		}
+	}
+	
+	//parent index
+	int parent(int child){
+		if(child != 0){
+			int idx = (child-1)/2;
+			return idx;
+		}
+		return -1;
+	}
+	
+	//left child index
+	int left(int parent){
+		int idx = (parent*2) + 1;
+		return idx < heap.size() ? idx : -1;
+	}
+
+	//right child index
+	int right(int parent){
+		int idx = (parent*2) + 2;
+		return idx < heap.size() ? idx : -1;
+	}
+};
+
+
+Node* rebuild_tree(vector<unsigned char>& table, const int end, int& i){
+	//check for trailing 0's followed by ending index
+	if( (i/8) >= end){
+		return NULL;
+	}
+	
+	for(int j=i; j<i+7; ++j){
+		if( table[j/8] & (1<<(7-(j%8))) > 0){
+			break;
+		}
+		if( (table[j/8] & (1<<(7-(j%8))) == 0) && ((j/8)+1) == end){
+			return NULL;
+		}
+	}
+	
+	if( (table[i/8] & (1<<(7-(i%8)))) > 0){
+		//create leaf
+		//read next 8 bits
+		i++;
+		char ch;
+		unsigned int b = 0;
+		for(int k=7; k>=0; --k){
+			int b_shift = 7 - (i%8);
+			unsigned int table_bit = (table[i/8] & (1<<b_shift)) > 0 ? 1 :0;
+			b |= table_bit << k;
+			i++;
+		}
+		ch = b;
+		Node* leaf = new Node(0,ch);
+		i--; //backtrack index
+		return leaf;
+	}else{
+		//create parent and leaf nodes
+		Node* parent = new Node(0, '\0');
+		parent->right_child = rebuild_tree(table, end, ++i);
+		parent->left_child = rebuild_tree(table, end, ++i);
+		return parent;
+	}
+}
+
+int main(int argc, char** argv){
+	//error handling for invalid arguements
+	if(argc != 2){
+		cerr << "Invalid number of arguements!" << endl;
+		return 0;
+	}
+	
+	//open file from arguements and create encoded .huf file
+	ifstream infile(argv[1], ios::binary);
+	ofstream outfile;
+	string outfile_name = argv[1];
+	outfile_name = outfile_name.substr(0, outfile_name.size()-4);
+	outfile.open(outfile_name.c_str(), ios::binary);
+
+	//rebuild huff tree
+	vector<unsigned char> buffer (
+		(istreambuf_iterator<char>(infile)), 
+		(istreambuf_iterator<char>()));
+
+	vector<unsigned char> table_buffer;
+	int eot = 0;
+	bool null_byte_clear = false;
+	for(int i=0; i<buffer.size(); ++i){
+		//checking that the beginning doesnt lead with a byte of 0's
+		if(!null_byte_clear && buffer[i] != '\0') {
+			null_byte_clear = true;
+		}
+		if(null_byte_clear && buffer[i] == '\0'){
+			eot = i+1;
+			break;
+		}
+		table_buffer.push_back(buffer[i]);
+	}
+	//rebuild huffman tree
+	int rebuild_index = 0;
+	Node* freq_heap = rebuild_tree(table_buffer, eot, rebuild_index);
+	Node* head = freq_heap;
+	
+	int byte_count = 0;
+	for(int i=eot; i<buffer.size(); ++i){
+		//cout << bitset<8>(buffer[i]) << " ";
+		for(int j=0; j<8; ++j){
+			int b = (buffer[i] & (1<<j)) > 0 ? 1 : 0;
+			if(b==1 && head->left_child){
+				head = head->left_child;
+			}else if( b==0 && head->right_child){
+				head = head->right_child;
+			}
+			if(head->byte_code != '\0'){
+				outfile.put(static_cast<char>(head->byte_code));
+				head = freq_heap;
+				byte_count++;
+			}
+		}
+	}
+
+	delete freq_heap;
+
+	infile.close();
+	outfile.close();
+
+	return 0;
+}
---
+++ henc.cpp
@@ -0,0 +1,225 @@
+//STEPHEN COLUCCIO cs435 9136 mp
+//Option 1: Huffman coding, encoding
+
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+class Node{
+public: 
+	int freq;
+	char byte_code;
+	Node * left_child;
+	Node * right_child;
+
+	Node(int f, char byte='\0', Node* left=NULL, Node* right=NULL): 
+		freq(f), byte_code(byte), left_child(left), right_child(right) {}
+	
+	~Node()
+	{
+		delete left_child;
+		delete right_child;
+	}
+	
+};
+
+
+//Priorty Heap
+class PriorityHeap{
+public:
+	vector<Node*> heap;
+
+	PriorityHeap(){}
+	~PriorityHeap(){}
+
+	void insert(Node* element){
+		heap.push_back(element);
+		heapify_up(heap.size()-1);
+	}
+
+	Node* extract_min(){
+		Node* min = heap.front();
+		heap[0] = heap.at(heap.size()-1);
+		heap.pop_back();
+		heapify_down(0);
+		return min;
+	}
+
+	void heapify_up(int idx){
+		while(parent(idx) >= 0 && idx > 0 && heap[parent(idx)]->freq > heap[idx]->freq){
+			Node* temp = heap[parent(idx)];
+			heap[parent(idx)] = heap[idx];
+			heap[idx] = temp;
+			idx = parent(idx);
+		}
+	}
+
+	void heapify_down(int idx){
+		int child = left(idx);
+		if(child > 0 && right(idx) > 0 && heap[child]->freq > heap[right(idx)]->freq){
+			child = right(idx);
+		} 
+		if(child > 0){
+			Node* temp = heap[idx];
+			heap[idx] = heap[child];
+			heap[child] = temp;
+			heapify_down(child);
+		}
+	}
+	
+	//parent index
+	int parent(int child){
+		if(child != 0){
+			int idx = (child-1)/2;
+			return idx;
+		}
+		return -1;
+	}
+	
+	//left child index
+	int left(int parent){
+		int idx = (parent*2) + 1;
+		return idx < heap.size() ? idx : -1;
+	}
+
+	//right child index
+	int right(int parent){
+		int idx = (parent*2) + 2;
+		return idx < heap.size() ? idx : -1;
+	}
+};
+
+//Write the construction of the frequency heap. 
+//Internal Node : 0
+//Leaf Node : 1 followed by char of node
+void write_freq_heap(Node* node, string& table_str){
+	if(node){
+		if(node->byte_code == '\0'){
+			table_str += "0";
+			write_freq_heap(node->right_child, table_str);
+			write_freq_heap(node->left_child, table_str);
+		}else{
+			table_str += "1";
+			table_str += (bitset<8>(node->byte_code)).to_string();
+		}
+	}
+}
+
+
+//encode bit pattern to be written to file
+void encode_bit_string(Node* node, unsigned char ch, string bit_str, vector<string>& local_bits){
+	if(node){
+		if(node->byte_code == ch && !node->right_child && !node->left_child){
+			local_bits[ch] = bit_str;
+		}else{
+			encode_bit_string(node->right_child, ch, bit_str+"0", local_bits);
+			encode_bit_string(node->left_child, ch, bit_str+"1", local_bits);
+		}
+	}
+}
+
+
+
+
+int main(int argc, char** argv){
+	
+	//error handling for invalid arguements
+	if(argc != 2){
+		cerr << "Invalid number of arguements!" << endl;
+		return 0;
+	}
+	
+	//open file from arguements and create encoded .huf file
+	ifstream infile(argv[1], ios::in | ios::binary);
+	vector<unsigned char> buffer (
+		(istreambuf_iterator<char>(infile)), 
+		(istreambuf_iterator<char>()));
+	
+	//create output file
+	ofstream outfile;
+	outfile.open(strcat(argv[1],".huf"), ios::binary);
+	
+	//determine frequency of bytes in provided file
+	vector<int> frequency(256);
+	for(int i=0; i<buffer.size(); ++i){
+		frequency[(int)buffer[i]]++;
+	}
+
+	//create priority heap
+	PriorityHeap* freq_heap = new PriorityHeap();
+	for(int i=0; i<frequency.size(); ++i){
+		if(frequency[i] != 0){
+			freq_heap->insert(new Node(frequency[i], i));
+		}
+	}
+
+	//build huffman tree
+	while(freq_heap->heap.size() > 1){
+		Node* right = freq_heap->extract_min();
+		Node* left = freq_heap->extract_min();
+		Node* parent_node = new Node(
+			right->freq + left->freq,
+			'\0', 
+			left, right);
+	
+		freq_heap->insert(parent_node);
+	}
+
+	//write huffman tree to encoded file
+	string table_str;
+	write_freq_heap(freq_heap->heap[0], table_str);
+	
+	unsigned char write_ch;
+	int bit_count=7;
+	for(int i=0; i<table_str.size(); ++i){
+		unsigned int bit = table_str[i] == '1' ? 1 : 0;
+		write_ch |= bit << bit_count;
+		if(bit_count == 0){
+			outfile.put(write_ch);
+			write_ch = '\0';
+			bit_count = 8;
+		}
+		bit_count--;
+		bit_count %= 8;
+	}
+	if(write_ch != '\0') outfile.put(write_ch);
+	//write a delimiter to know end of huff tree
+	outfile.put('\0');
+
+	//create huffman codes
+	vector<string> huff_codes(256);
+	unsigned char ch;
+	for(int i=0; i<256; ++i){
+		huff_codes[i] = "";
+		ch = static_cast<unsigned char>(i);
+		encode_bit_string(freq_heap->heap[0], ch, "", huff_codes);
+	}
+
+	//write huffman codes to outfile
+	unsigned char encoded_ch;
+	for(int i=0; i<buffer.size(); ++i){
+		ch = buffer[i];
+		int bit_count;
+		for(unsigned int j=0; j<huff_codes[ch].size(); ++j){
+			unsigned int bit_str = huff_codes[ch].at(j) == '1' ? 1 : 0;
+			encoded_ch |= bit_str << bit_count;
+			bit_count++;
+			bit_count %= 8;
+			if(bit_count == 0){
+				outfile.put(static_cast<char>(encoded_ch));
+				encoded_ch = '\0';
+			}
+		}
+	}
+	//write the remaining bits if they exist
+	if(encoded_ch != '\0') outfile.put(encoded_ch);
+	
+	infile.close();
+	outfile.close();
+
+	return 0;
+}
